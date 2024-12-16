4bbd18645f2c7899c759197c07c7c18697af2d5f
seungjae <dhtmdwo73@naver.com>
2024-12-16 19:25:39 +0900

    좋아요 한 식당 기능

--- js/layout.js
+++ js/layout.js
@@ -8,6 +8,7 @@ function loadHTML(url, elementId, callback) {
     .catch((error) => {
       console.error(`Error loading ${url}:`, error);
     });
+
 }
 
 loadHTML("/header.html", "header", function () {
@@ -23,14 +24,16 @@ loadHTML("/header.html", "header", function () {
   }
 });
 
-loadHTML("/footer.html", "footer", function () {});
+loadHTML("/footer.html", "footer", function () { });
+
 
 document.addEventListener("DOMContentLoaded", function () {
   if (document.getElementById("client_sidebar")) {
     loadHTML(
       "/mypage/mypage_client_sidebar.html",
       "client_sidebar",
-      function () {}
+      function () { }
     );
   }
 });
+
