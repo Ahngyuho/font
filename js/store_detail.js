76356629ad291f16b2d10cc1fbf97ec2bcd8dd46
yeon7485 <wsx4379@naver.com>
2024-12-18 21:25:09 +0900

    [Docs] 식당 상세 페이지 반응형 구현현

--- js/store_detail.js
+++ js/store_detail.js
@@ -5,6 +5,11 @@ contentBoxes[0].style.display = "block";
 
 for (let submenu of submenuList) {
   submenu.addEventListener("click", () => {
+    submenuList.forEach((menu) => { 
+      menu.style.fontWeight = "400";
+      menu.style.color = "#1A1A1A";
+
+    })
     contentBoxes.forEach((box) => {
       box.style.display = "none";
     });
@@ -12,5 +17,7 @@ for (let submenu of submenuList) {
     let target = submenu.getAttribute("data-target");
     let boxElement = document.getElementById(target);
     boxElement.style.display = "block";
+    submenu.style.color = "#FF7400";
+    submenu.style.fontWeight = "700";
   });
 }
